32dc9fd516700879184dd9579a78bb38db782842
heebp <91429968+heebp@users.noreply.github.com>
2024-05-20 21:50:50 +0900

    [level 3] Title: 베스트앨범, Time: 0.66 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,46 +3,48 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
+        HashMap<String, Integer> genreMap = new HashMap<>();
+        
+        for(int i =0; i< genres.length; i++){
+            genreMap.put(genres[i], genreMap.getOrDefault(genres[i], 0) + plays[i]);
+        }
+        
+        List<String> genreList = new ArrayList<>(genreMap.keySet());
+        Collections.sort(genreList, (o1, o2) -> (genreMap.get(o2).compareTo(genreMap.get(o1))));
+        
+        int temp = 0;
         List<Integer> answerList = new ArrayList<>();
-        Map<String, Integer> genresPlay = new HashMap<>();
-        Map<String, List<Integer[]>> map = new HashMap<>();
-        for(int i = 0; i < genres.length; i++){
-            genresPlay.put(genres[i], genresPlay.getOrDefault(genres[i], 0) + plays[i]);
-            
-            List<Integer[]> lists = map.getOrDefault(genres[i], new ArrayList<>());
-            lists.add(new Integer[]{plays[i], i});
-            map.put(genres[i], lists);
-        }
-        
-        for(String key : map.keySet()){
-            List<Integer[]> lists = map.get(key);
-            Collections.sort(lists, new Comparator<Integer[]>(){
-               
-                @Override
-                public int compare(Integer[] o1, Integer[] o2){
-                    return o2[0] - o1[0];
+        for(String genre : genreList ){
+            int max  = 0;
+            int second  = 0;
+            int max_i = -1;
+            int sec_i = -1;
+	        for(int i = 0; i < genres.length; i++){
+                if(genre.equals(genres[i]) && plays[i] >= max){
+                    second = max;
+                    sec_i = max_i;
+                    max = plays[i];
+                    max_i = i;
+                }else if( genre.equals(genres[i]) && second <= plays[i] && plays[i] < max){
+                    second = plays[i];
+                    sec_i = i;
                 }
-            });
-        }
-        
-        List<String> orderGenre = new ArrayList<>(genresPlay.keySet());
-        Collections.sort(orderGenre, new Comparator<String>(){
-           
-            @Override
-            public int compare(String o1, String o2){
-                return genresPlay.get(o2) - genresPlay.get(o1);
             }
-        });
-        for(String genre : orderGenre){
-            List<Integer[]> play = map.get(genre);
-            answerList.add(play.get(0)[1]);
-            if(play.size() > 1)
-                answerList.add(play.get(1)[1]);
+            if( max == second && max_i > sec_i){
+                temp = max_i;
+                max_i = sec_i;
+                sec_i = temp;
+            }
+            if(max_i != -1 ){
+                answerList.add(max_i);
+            }
+            if(sec_i != -1){
+                answerList.add(sec_i);
+            }
+            max = 0;
+            second = 0;
         }
-        answer = new int[answerList.size()];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = answerList.get(i);
-        }
+        answer = answerList.stream().mapToInt(i->i).toArray();
         return answer;
     }
 }
