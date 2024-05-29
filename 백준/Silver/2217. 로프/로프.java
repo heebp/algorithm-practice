0a790861397d0c97cc87998827c3a6e28b8ab3ce
heebp <91429968+heebp@users.noreply.github.com>
2024-05-30 00:15:33 +0900

    [Silver IV] Title: 로프, Time: 400 ms, Memory: 26604 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        Integer[] rope = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            rope[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(rope, (o1, o2) -> o2 - o1);
+        int max = 0;
+        int idx = 0;
+        while(idx < N){
+            int w = rope[idx] * (idx + 1);
+            if(max < w)
+                max = w;
+            idx++;
+        }
+        System.out.println(max);
+    }
+}
