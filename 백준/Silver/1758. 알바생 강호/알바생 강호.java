ce237cca5f95c0b2c9fadcd93051ed781dc1d949
heebp <91429968+heebp@users.noreply.github.com>
2024-05-30 23:33:36 +0900

    [Silver IV] Title: 알바생 강호, Time: 276 ms, Memory: 24996 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[N];
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr, (o1, o2) -> o2 - o1);
+        long max = 0;
+        for (int i = 0; i < N; i++) {
+            int tip = arr[i] - i;
+            if (tip <= 0) {
+                break;
+            }
+            max += tip;
+        }
+        System.out.println(max);
+    }
+}
