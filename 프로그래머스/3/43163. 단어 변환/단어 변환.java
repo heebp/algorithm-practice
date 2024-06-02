0e9afef2774ba46ec6ff41890ce44f1d985e8c26
heebp <91429968+heebp@users.noreply.github.com>
2024-06-02 14:56:23 +0900

    [level 3] Title: 단어 변환, Time: 0.09 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,45 +1,41 @@
 import java.util.*;
 
 class Solution {
+    static boolean[] visited;
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
+        visited = new boolean[words.length];
         
-        answer = bfs(begin, target, words, new boolean[words.length]);
+        answer = bfs(begin, target, words, visited);
         return answer;
     }
     int bfs(String begin, String target, String[] words, boolean[] visited){
         Queue<int[]> q = new LinkedList<>();
-        for(int nextWord : findAllNextWord(begin, words, visited)){
-            q.add(new int[]{nextWord, 1});
-        }
+        add(begin, words, q, 0);
         while(!q.isEmpty()){
             int[] index = q.poll();
             String word = words[index[0]];
             if(word.equals(target))
                 return index[1];
-            List<Integer> nextWords = findAllNextWord(word, words, visited);
-            for(int nextWord : nextWords){
-                q.add(new int[]{nextWord, index[1] + 1});
-                visited[nextWord] = true;
-            }
+            add(word, words, q, index[1]);
         }
         return 0;
     }
     
-    List<Integer> findAllNextWord(String word, String[] words, boolean[] visited){
-        List<Integer> list = new ArrayList<>();
+    void add(String word, String[] words, Queue<int[]> q, int cnt){
+        int length = word.length();
         for(int i = 0; i < words.length; i++){
             if(visited[i])
                 continue;
             int hit = 0;
-            for(int j = 0; j < word.length(); j++){
+            for(int j = 0; j < length; j++){
                 if(word.charAt(j) == words[i].charAt(j))
                     hit++;
             }
-            if(word.length() == hit + 1){
-                list.add(i);
+            if(length == hit + 1){
+                visited[i] = true;
+                q.add(new int[]{i, cnt + 1});
             }
         }
-        return list;
     }
 }
