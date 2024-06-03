c9c768027c24fc2d13d736d9820b505a8f2141f5
heebp <91429968+heebp@users.noreply.github.com>
2024-06-04 01:39:23 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,48 +1,34 @@
-
-import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     public static void main(String[] args) throws IOException {
-        List<Integer> list = new LinkedList<>();
-        List<Character> ch = new LinkedList<>();
-        String s = br.readLine();
-        int len = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-                list.add(Integer.valueOf((s.substring(len, i))));
-                ch.add(s.charAt(i));
-                len = i+1;
-            }
-            if (i == s.length()-1) {
-                list.add(Integer.valueOf(s.substring(len,i+1)));
+        String input = br.readLine();
+        String[] convert = input.split("\\+");
+        List<Integer> num = new ArrayList<>();
+        for(String s : convert){
+            String[] nums = s.split("-");
+            for (String sNum : nums) {
+                num.add(Integer.parseInt(sNum));
             }
         }
-        int result;
-        int operand = '+';
-        while (ch.size() != 0) {
-            for (int i = 0; i < ch.size(); i++) {
-                if (ch.get(i) == operand) {
-                    if (ch.get(i) == '+') {
-                        result = list.get(i) + list.get(i + 1);
-                    } else {
-                        result = list.get(i) - list.get(i + 1);
-                    }
-                    list.remove(i);
-                    ch.remove(i);
-                    list.remove(i);
-                    list.add(i, result);
-                    i--;
-                }
-            }
-            operand = '-';
+        int delimIdx = 1;
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == '-')
+                break;
+            if(input.charAt(i) == '+')
+                delimIdx++;
         }
-        bw.write(list.get(0)+"");
-        bw.flush();
-        bw.close();
+        int min = 0;
+        for (int i = 0; i < num.size(); i++) {
+            if(i < delimIdx)
+                min += num.get(i);
+            else
+                min -= num.get(i);
+        }
+        System.out.println(min);
     }
 }
