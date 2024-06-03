30a4e6ee1852fdf1abc0ccdf3418cf49ec0e068c
heebp <91429968+heebp@users.noreply.github.com>
2024-06-04 01:17:09 +0900

    [Silver I] Title: 회의실 배정, Time: 520 ms, Memory: 43220 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,61 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-
-import java.io.*;
-import java.util.*;
-
-//1931
 public class Main {
-    static class Schedule{
-        int s;
-        int e;
-
-        Schedule(int s, int e) {
-            this.s =s ;
-            this.e =e;
-        }
-    }
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
     public static void main(String[] args) throws IOException {
-        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        List<Schedule> list = new ArrayList<Schedule>();
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            list.add(new Schedule(s, e));
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        list.sort((o1, o2) -> o1.s != o2.s ? o1.s - o2.s : o1.e - o2.e);
-        for (int i = 0; i < list.size(); i++) {
-
-            if(i+1 == list.size()){
-                break;
-            }
-            if (list.get(i).s == list.get(i).e) {
-                continue;
-            }
-            if (list.get(i).s <= list.get(i + 1).s && list.get(i).e == list.get(i + 1).e && list.get(i+1).s == list.get(i+1).e) {
-                continue;
-            }
-            if (list.get(i).s <= list.get(i + 1).s && list.get(i).e >= list.get(i + 1).e) {
-                list.remove(i);
-                i--;
-                continue;
-            }
-            if (list.get(i).s >= list.get(i + 1).s && list.get(i).e <= list.get(i + 1).e) {
-                list.remove(i + 1);
-                i--;
-                continue;
-            }
-            if (list.get(i).s < list.get(i + 1).s && list.get(i).e < list.get(i + 1).e && list.get(i+1).s < list.get(i).e) {
-                list.remove(i + 1);
-                i--;
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[1] == o2[1])
+                return o1[0] - o2[0];
+            return o1[1] - o2[1];
+        });
+        int cnt = 0;
+        int end = -1;
+        for(int[] schedule : arr){
+            if(end <= schedule[0]){
+                end = schedule[1];
+                cnt++;
             }
         }
-        bw.write(list.size()+"");
-        bw.flush();
-        bw.close();
+        System.out.println(cnt);
     }
 }
