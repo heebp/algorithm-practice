4518e63c0421dc2ba65c9c8cb50d993c751f0890
heebp <91429968+heebp@users.noreply.github.com>
2024-06-10 22:22:40 +0900

    [Gold III] Title: 나머지 합, Time: 664 ms, Memory: 184820 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        long[] sum = new long[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        sum[0] = arr[0];
+        for (int i = 1; i < N; i++)
+            sum[i] = arr[i] + sum[i - 1];
+
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < N; i++)
+            map.put(sum[i] % M, map.getOrDefault(sum[i] % M, 0L) + 1);
+
+        long res = map.getOrDefault(0L, 0L);
+        for (Map.Entry<Long, Long> entry : map.entrySet()) {
+            long n = entry.getValue();
+            if(n > 1)
+                res += n * (n - 1) / 2;
+        }
+        System.out.println(res);
+    }
+}
