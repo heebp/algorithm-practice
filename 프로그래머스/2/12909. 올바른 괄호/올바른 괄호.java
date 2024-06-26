66ae54ddc26281d0ad5ae37091e533b66768a181
heebp <91429968+heebp@users.noreply.github.com>
2024-06-26 23:15:33 +0900

    [level 2] Title: 올바른 괄호, Time: 13.42 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(')
+                sb.append('(');
+            else{
+                if(sb.length() == 0)
+                    return false;
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+        return sb.length() == 0;
+    }
+}
