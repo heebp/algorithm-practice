ed0bfbf95edd8defc43420b2c24000a7803b1b28
heebp <91429968+heebp@users.noreply.github.com>
2024-07-09 23:34:57 +0900

    [level 2] Title: 기능개발, Time: 0.32 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        int curTime = (int)Math.ceil((double)(100 - progresses[0]) / speeds[0]);
+        int prev  = 0;
+        for(int i = 1; i < progresses.length; i++){
+            int time = (int)Math.ceil((double)(100 - progresses[i]) /speeds[i]);
+            if(curTime < time){
+                curTime = time;
+                list.add(i - prev);   
+                prev = i;
+            }
+        }
+        list.add(progresses.length - prev);
+        answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++)
+            answer[i] = list.get(i);
+        return answer;
+    }
+}
