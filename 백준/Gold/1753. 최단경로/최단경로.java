3b633acfe3cf8feb171130df74b1c4612c72bcbc
heebp <91429968+heebp@users.noreply.github.com>
2024-08-22 18:41:20 +0900

    [Gold IV] Title: 최단경로, Time: 748 ms, Memory: 113356 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,23 +1,9 @@
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static boolean[] visited;
-    static int[] shortest;
-    static class Node {
-        int v;
-        int w;
-        Node(int v, int w){
-            this.v = v;
-            this.w = w;
-        }
-    }
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st;
@@ -26,54 +12,48 @@ public class Main {
         int E = Integer.parseInt(st.nextToken());
         int start = Integer.parseInt(br.readLine());
 
-        visited = new boolean[V + 1];
-        shortest = new int[V + 1];
-        for (int i = 0; i < V + 1; i++) {
-            shortest[i] = Integer.MAX_VALUE;
-        }
-        List<List<Node>> graph = new ArrayList<>();
-        shortest[start] = 0;
-        for (int i = 0; i < V + 1; i++) {
+        List<List<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i <= V; i++)
             graph.add(new ArrayList<>());
-        }
 
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-            graph.get(u).add(new Node(v, w));
+            graph.get(u).add(new int[]{v, w});
         }
-        for (int i = 0; i < V; i++) {
-            int min = Integer.MAX_VALUE;
-            int index = 0;
-
-            for (int j = 1; j < V + 1; j++) {
-                if(!visited[j]){
-                    if (shortest[j] < min) {
-                        min = shortest[j];
-                        index = j;
-                    }
-                }
-            }
-
-            visited[index] = true;
-            for (int j = 0; j < graph.get(index).size(); j++) {
-                Node node = graph.get(index).get(j);
-
-                if(shortest[node.v] > shortest[index] + node.w){
-                    shortest[node.v] = shortest[index] + node.w;
-                }
-            }
-        }
-        for (int i = 1; i < V + 1; i++) {
-            if (shortest[i] < Integer.MAX_VALUE) {
-                bw.write(shortest[i]+"\n");
-            } else {
+        int[] vs = dijkstra(graph, start);
+        for (int i = 1; i < vs.length; i++) {
+            if(vs[i] == Integer.MAX_VALUE)
                 bw.write("INF\n");
-            }
+                else bw.write(vs[i] + "\n");
         }
         bw.flush();
         bw.close();
+    }
+    static int[] dijkstra(List<List<int[]>> graph, int start) {
+        int[] v = new int[graph.size()];
+        Arrays.fill(v, Integer.MAX_VALUE);
+        v[start] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        pq.add(new int[]{start, 0});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if(cur[1] < v[cur[0]])
+                continue;
+            for (int[] next : graph.get(cur[0])) {
+                if(v[next[0]] <= v[cur[0]] + next[1])
+                    continue;
+                v[next[0]] = v[cur[0]] +  next[1];
+                pq.add(new int[]{next[0], v[next[0]]});
+            }
+        }
+        return v;
     }
 }
