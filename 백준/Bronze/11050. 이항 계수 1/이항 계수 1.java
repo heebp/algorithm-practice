8cec6f76a209ea00c236946ccc1930ced8b74321
heebp <91429968+heebp@users.noreply.github.com>
2024-09-08 20:49:32 +0900

    [Bronze I] Title: 이항 계수 1, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] mul = new int[11];
+        mul[0] = 1;
+        for(int i = 1; i < 11; i++){
+            mul[i] = mul[i - 1] * i;
+        }
+        System.out.println(k == 0 ? 1 : mul[n] / mul[n - k] / mul[k]);
+        br.close();
+    }
+}
