adc7f1d232e2ff8537fa37e24805cbc296f5da58
heebp <91429968+heebp@users.noreply.github.com>
2024-11-29 02:52:57 +0900

    [level 2] Title: 삼각 달팽이, Time: 44.46 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,53 +1,45 @@
 import java.util.*;
 
 class Solution {
-    static int[] dr = {1, 0, -1};
-    static int[] dc = {0, 1, -1};
-    //삼각형을 배열 형태(사각형)으로 구현 => 배열에선 직각 삼각형
-    
-    //0,0부터 아래 방향 진행
-    
-    //아래가 막혀있을때,(index out, num 존재) 진행 방향을 반시계 방향으로 바꿈
-    
-    //종료 조건 : 방향 전환 시에 바로 막혀있는 경우 종료 
+    static int[] dx = {0, 1, -1};
+    static int[] dy = {1, 0, -1};
     public int[] solution(int n) {
-        int[][] map = new int[n][n];
-        int[] pos = {0,0};
-        int dir = 0;
-        int cnt = 1;
-        int r = 0;
-        int c = 0;
+        int[] answer = {};
+        int[][] triangle = new int[n][n];
+        int x = 0;
+        int y = 0;
+        int index = 0;
+        int value = 1;
+        int max = (n * (n + 1)) / 2;
         while(true){
-            if(r < 0 || c < 0 || r >= n || c >= n)
+            triangle[x][y] = value++;            
+            int nx = x + dx[index];
+            int ny = y + dy[index];
+            if(nx == n || ny == n || triangle[nx][ny] != 0){
+                index = (index + 1) % 3;
+                nx = x + dx[index];
+                ny = y + dy[index];
+            }
+            if(max < value)
                 break;
-            map[r][c] = cnt;
-            int nr = r + dr[dir];
-            int nc = c + dc[dir];
-            
-            if(nr < 0 || nc < 0 || nr >= n || nc >= n || map[nr][nc] != 0){
-                dir = (dir + 1) % 3;
-                nr = r + dr[dir];
-                nc = c + dc[dir];
-                if(nr >= 0 && nc >= 0 && nr < n && nc < n && map[nr][nc] != 0)
-                    break;
-            }
-            
-            r = nr;
-            c = nc;
-            cnt++;
-        }
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(map[i][j] == 0)
-                    break;
-                list.add(map[i][j]);
-            }
+            x = nx;
+            y = ny;
         }
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++)
-            answer[i] = list.get(i);
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(triangle[j][i] != 0)
+                    arr.add(triangle[j][i]);
+                else
+                    break;
+            }
+        }  
+        answer = new int[max];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+        
         return answer;
     }
 }
