55e17a0b74f6447ee70b10eb3477ff67f6201973
heebp <91429968+heebp@users.noreply.github.com>
2024-12-19 01:23:49 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.18 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,14 +1,6 @@
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        long[] dp = new long[n + 3];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        for(int i = 4; i <= n; i++){
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
-        }
-        answer = dp[n] % 1234567;
         return answer;
     }
 }
