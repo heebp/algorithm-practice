e85be78a169b7b4ffb11ea914c25b78da8aee9d4
heebp <91429968+heebp@users.noreply.github.com>
2024-12-25 02:11:20 +0900

    [level 2] Title: 땅따먹기, Time: 15.15 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,18 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        for(int i = 1; i < land.length; i++){
+            for(int j = 0; j < land[0].length; j++){
+                int localMax = 0;
+                for(int k = 0; k < land[0].length; k++){
+                    if(j == k)
+                        continue;
+                    localMax = Math.max(localMax, land[i - 1][k]);
+                }
+                land[i][j] += localMax;
+                answer = Math.max(answer, land[i][j]);
+            }
+        }
+        return answer;
+    }
+}
